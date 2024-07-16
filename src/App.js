4450a9c7691f7ce385c3e4d6d20ce1e4dcde95aa
Prachi Kane <prachikane27@gmail.com>
2024-07-15 20:53:00 -0500

    style change

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navigation/>
-      <div>
-        <HomePage/>
-      </div>
+      <HomePage/>
     </div>
   );
 }
--- src/Customer/Components/HomeCarousel/index.jsx
+++ src/Customer/Components/HomeCarousel/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import { HomeCarousalData } from './data';
-//import styles from './index.scss';
+import './index.css';
 
 const items = HomeCarousalData.map((item) =>
     <img className='cursor-pointer' role='presentation' src={item.image} alt=""></img>
@@ -10,7 +10,13 @@ const items = HomeCarousalData.map((item) =>
 
 const HomeCarousel = () => {
     return(
-        <AliceCarousel items={items}/>
+        <AliceCarousel 
+            items={items}
+            disableButtonsControls
+            autoPlay
+            autoPlayInterval={2000}
+            infinite
+        />
     )
 };
 
